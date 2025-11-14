b60bc5c604892330538fd9cfaa2834d2b4039836
Torstein Tenstad <torstein@tenstad.com>
2025-11-14 15:29:05 +0100

    Improve top-level state machine

--- src/play_state.rs
+++ src/play_state.rs
@@ -1,7 +1,7 @@
 use crate::{
-    data_model::{Ability, Action, Card, Character},
+    data_model::{Action, ActionMovement, ActionOnSelf, ActionTargeted, CardData, Character},
     play::{end_turn, play_card_unchecked},
-    precondition::precondition_is_met,
+    pop_ability::pop_ability_ignore_unsatisfied,
     resolve_action::{
         ActionInputMovement, ActionInputOnSelf, ActionInputTargeted, resolve_action_movement,
         resolve_action_on_self, resolve_action_targeted,
@@ -9,51 +9,36 @@ use crate::{
     single_out::single_out,
 };
 
-#[derive(Default, Debug)]
-pub struct CharacterAbilityState {
-    remaining_abilities: Vec<Ability>,
-    remaining_actions: Vec<Action>,
-    cancelable: bool,
-}
-
-pub struct InputState {}
-
 pub enum PlayCardOrEndTurn {
-    PlayCard(Card),
+    PlayCard(CardData),
     EndTurn,
 }
 
-impl InputState {
-    pub fn advance_on_self(&mut self) -> Option<ActionInputOnSelf> {
-        todo!()
-    }
-    pub fn advance_targeted(&mut self) -> Option<ActionInputTargeted> {
-        todo!()
-    }
-    pub fn advance_movement(&mut self) -> Option<ActionInputMovement> {
-        todo!()
-    }
-    pub fn advance_on_self_cancelable(&mut self) -> Option<Option<ActionInputOnSelf>> {
-        todo!()
-    }
-    pub fn advance_targeted_cancelable(&mut self) -> Option<Option<ActionInputTargeted>> {
-        todo!()
-    }
-    pub fn advance_movement_cancelable(&mut self) -> Option<Option<ActionInputMovement>> {
-        todo!()
-    }
-    pub fn advance_play_card_or_end_turn(&mut self) -> Option<PlayCardOrEndTurn> {
-        todo!()
-    }
+pub enum Cancelable<T> {
+    Some(T),
+    Canceled,
+}
+
+pub enum PendingInput<T> {
+    Some(T),
+    Pending,
+}
+
+#[rustfmt::skip]
+pub trait Input {
+    fn poll_action_input_on_self(&mut self, action: &ActionOnSelf) -> PendingInput<ActionInputOnSelf>;
+    fn poll_action_input_targeted(&mut self, action: &ActionTargeted) -> PendingInput<ActionInputTargeted>;
+    fn poll_action_input_movement(&mut self, action: &ActionMovement) -> PendingInput<ActionInputMovement>;
+    fn poll_action_input_on_self_cancelable(&mut self, action: &ActionOnSelf) -> PendingInput<Cancelable<ActionInputOnSelf>>;
+    fn poll_action_input_targeted_cancelable(&mut self, action: &ActionTargeted) -> PendingInput<Cancelable<ActionInputTargeted>>;
+    fn poll_action_input_movement_cancelable(&mut self, action: &ActionMovement) -> PendingInput<Cancelable<ActionInputMovement>>;
+    fn poll_play_card_or_end_turn(&mut self) -> PendingInput<PlayCardOrEndTurn>;
 }
 
 #[derive(Debug, Default)]
 pub struct PlayState {
-    characters: Vec<Character>,
     active: usize,
     has_turn: usize,
-    last_character_performing_action: usize,
-    non_turn_abilities_unresolved: bool,
     remaining_actions: Vec<Action>,
     cancelable: bool,
 }
@@ -64,97 +49,69 @@ enum ExecutionState {
     Canceled,
 }
 
-pub fn game_loop() {
-    let mut input_state = InputState {};
-    let mut play_state = PlayState::default();
-    loop {
-        match single_out(&mut play_state.characters, play_state.active) {
-            None => {
-                debug_assert!(false);
-                play_state.active = 0;
-            }
-            Some((active_character, mut characters)) => {
-                match play_state.remaining_actions.first() {
-                    Some(action) => {
-                        let execution_state = execute_action(
-                            action,
-                            play_state.cancelable,
-                            &mut input_state,
-                            active_character,
-                            &mut characters,
-                        );
+pub fn step_play_state(
+    play_state: &mut PlayState,
+    input: &mut impl Input,
+    all_characters: &mut [Character],
+) {
+    let Some((active_character, mut characters)) = single_out(all_characters, play_state.active)
+    else {
+        debug_assert!(false);
+        return;
+    };
 
-                        match execution_state {
-                            ExecutionState::Executed => {
-                                play_state.remaining_actions.pop();
-                                play_state.non_turn_abilities_unresolved = true;
-                                play_state.last_character_performing_action = play_state.active;
-                                play_state.cancelable = false;
-                            }
-                            ExecutionState::Canceled => {
-                                play_state.remaining_actions.clear();
-                            }
-                            ExecutionState::Waiting => {}
-                        }
+    match play_state.remaining_actions.first() {
+        Some(action) => {
+            let execution_state = execute_action(
+                action,
+                play_state.cancelable,
+                input,
+                active_character,
+                &mut characters,
+            );
+
+            match execution_state {
+                ExecutionState::Executed => {
+                    play_state.remaining_actions.pop();
+                    play_state.cancelable = false;
+                }
+                ExecutionState::Canceled => {
+                    play_state.remaining_actions.clear();
+                }
+                ExecutionState::Waiting => {}
+            }
+        }
+        None => {
+            let last_active = play_state.active;
+            loop {
+                play_state.active = (play_state.active + 1) % all_characters.len();
+
+                let Some((active_character, characters)) =
+                    single_out(all_characters, play_state.active)
+                else {
+                    debug_assert!(false);
+                    break;
+                };
+
+                match pop_ability_ignore_unsatisfied(active_character, &characters) {
+                    Some(actions) => {
+                        play_state.remaining_actions = actions;
+                        play_state.cancelable = true;
+                        break;
                     }
                     None => {
-                        if play_state.non_turn_abilities_unresolved {
-                            play_state.active =
-                                (play_state.active + 1) % play_state.characters.len();
-                            loop {
-                                let (active_character, characters) =
-                                    single_out(&mut play_state.characters, play_state.active)
-                                        .unwrap();
-                                match active_character.remaining_abilities.pop() {
-                                    Some(ability) => {
-                                        if ability.precondition.is_none_or(|precondition| {
-                                            precondition_is_met(
-                                                &precondition,
-                                                &characters,
-                                                active_character,
-                                            )
-                                        }) {
-                                            play_state.remaining_actions = ability.actions;
-                                            play_state.cancelable = true;
-                                        }
-                                    }
-                                    None => {
-                                        play_state.active =
-                                            (play_state.active + 1) % play_state.characters.len();
-                                    }
+                        if play_state.active == last_active {
+                            play_state.active = play_state.has_turn;
+                            match input.poll_play_card_or_end_turn() {
+                                PendingInput::Some(PlayCardOrEndTurn::PlayCard(card_data)) => {
+                                    play_card_unchecked(active_character, card_data); // TODO: checks
                                 }
-                            }
-                        } else {
-                            match active_character.remaining_abilities.pop() {
-                                Some(ability) => {
-                                    if ability.precondition.is_none_or(|precondition| {
-                                        precondition_is_met(
-                                            &precondition,
-                                            &characters,
-                                            active_character,
-                                        )
-                                    }) {
-                                        play_state.remaining_actions = ability.actions;
-                                        play_state.cancelable = true;
-                                    }
+                                PendingInput::Some(PlayCardOrEndTurn::EndTurn) => {
+                                    end_turn(active_character);
+                                    play_state.has_turn =
+                                        (play_state.has_turn + 1) % all_characters.len()
                                 }
-                                None => {
-                                    if play_state.active == play_state.has_turn {
-                                        match input_state.advance_play_card_or_end_turn() {
-                                            Some(PlayCardOrEndTurn::PlayCard(card)) => {
-                                                play_card_unchecked(active_character, card.data()); // TODO: checks
-                                            }
-                                            Some(PlayCardOrEndTurn::EndTurn) => {
-                                                end_turn(active_character);
-                                                play_state.has_turn = (play_state.has_turn + 1)
-                                                    % play_state.characters.len()
-                                            }
-                                            None => {}
-                                        }
-                                    } else {
-                                        todo!()
-                                    }
-                                }
+                                PendingInput::Pending => {}
                             }
                         }
                     }
@@ -167,7 +124,7 @@ pub fn game_loop() {
 fn execute_action<C>(
     action: &Action,
     cancelable: bool,
-    input_state: &mut InputState,
+    input_state: &mut impl Input,
     active_character: &mut Character,
     characters: &mut C,
 ) -> ExecutionState
@@ -179,43 +136,50 @@ where
             resolve_action_on_self(action, active_character, characters);
             ExecutionState::Executed
         }
-        (Action::OnSelf(action), true) => match input_state.advance_on_self_cancelable() {
-            Some(Some(_)) => {
-                resolve_action_on_self(action, active_character, characters);
-                ExecutionState::Executed
+        (Action::OnSelf(action), true) => {
+            match input_state.poll_action_input_on_self_cancelable(action) {
+                PendingInput::Some(Cancelable::Some(_)) => {
+                    resolve_action_on_self(action, active_character, characters);
+                    ExecutionState::Executed
+                }
+                PendingInput::Some(Cancelable::Canceled) => ExecutionState::Canceled,
+                PendingInput::Pending => ExecutionState::Waiting,
             }
-            Some(None) => ExecutionState::Canceled,
-            None => ExecutionState::Waiting,
-        },
-        (Action::Targeted(action), false) => match input_state.advance_targeted() {
-            Some(input) => {
+        }
+        (Action::Targeted(action), false) => match input_state.poll_action_input_targeted(action) {
+            PendingInput::Some(input) => {
                 resolve_action_targeted(action, &input, active_character, characters);
                 ExecutionState::Executed
             }
-            None => ExecutionState::Waiting,
+            PendingInput::Pending => ExecutionState::Waiting,
         },
-        (Action::Targeted(action), true) => match input_state.advance_targeted_cancelable() {
-            Some(Some(input)) => {
-                resolve_action_targeted(action, &input, active_character, characters);
-                ExecutionState::Executed
+        (Action::Targeted(action), true) => {
+            match input_state.poll_action_input_targeted_cancelable(action) {
+                PendingInput::Some(Cancelable::Some(input)) => {
+                    resolve_action_targeted(action, &input, active_character, characters);
+                    ExecutionState::Executed
+                }
+
+                PendingInput::Some(Cancelable::Canceled) => ExecutionState::Canceled,
+                PendingInput::Pending => ExecutionState::Waiting,
             }
-            Some(None) => ExecutionState::Canceled,
-            None => ExecutionState::Waiting,
-        },
-        (Action::Movement(action), false) => match input_state.advance_movement() {
-            Some(input) => {
+        }
+        (Action::Movement(action), false) => match input_state.poll_action_input_movement(action) {
+            PendingInput::Some(input) => {
                 resolve_action_movement(action, &input, active_character);
                 ExecutionState::Executed
             }
-            None => ExecutionState::Waiting,
+            PendingInput::Pending => ExecutionState::Waiting,
         },
-        (Action::Movement(action), true) => match input_state.advance_movement_cancelable() {
-            Some(Some(input)) => {
-                resolve_action_movement(action, &input, active_character);
-                ExecutionState::Executed
+        (Action::Movement(action), true) => {
+            match input_state.poll_action_input_movement_cancelable(action) {
+                PendingInput::Some(Cancelable::Some(input)) => {
+                    resolve_action_movement(action, &input, active_character);
+                    ExecutionState::Executed
+                }
+                PendingInput::Some(Cancelable::Canceled) => ExecutionState::Canceled,
+                PendingInput::Pending => ExecutionState::Waiting,
             }
-            Some(None) => ExecutionState::Canceled,
-            None => ExecutionState::Waiting,
-        },
+        }
     }
 }
